5432e3a3e0ecc665796c905c866386d33dfac9c5
Denuwan <denuwan555umayanga@gmail.com>
2025-04-30 23:28:53 +0530

    Fixed ClothesCard

--- FrontEnd/web/src/components/ClothesCard.jsx
+++ FrontEnd/web/src/components/ClothesCard.jsx
@@ -17,7 +17,12 @@ const ClothesCard = ({ item }) => {
         } 
       </div>
     </div>
-      <div clas
+      <div className="p-4">
+        <h2 className="text-lg font-semibold text-gray-800">{item.name}</h2>
+        <p className="text-gray-600 mt-2">${item.price}</p>
+        <Link to={item.viewDetails} className="px-2 py-1 bg-gradient-to-r from-green-300 via-teal-300 to-cyan-400 rounded-lg text-black">
+          View Product
+        </Link>
       </div>
     </div>
   );
